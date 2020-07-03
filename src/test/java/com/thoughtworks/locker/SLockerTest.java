b56b5a5c833effefa64715262788537249686347
zhengxiongli <xlzheng@thoughtworks.com>
2020-07-03 20:39:33 +0800

    zhengxiongli LLocker取包失败

--- src/test/java/com/thoughtworks/locker/SLockerTest.java
+++ src/test/java/com/thoughtworks/locker/SLockerTest.java
@@ -74,9 +74,16 @@ public class SLockerTest {
     }
 
     @Test(expected = UnknownOptionException.class)
-    public void should_store_fail_when_Llocker_store_bag_given_LLokcer() {
+    public void should_store_fail_when_Llocker_store_bag_given_Lokcer() {
         LLocker lLocker = new LLocker(1);
 
         lLocker.store(new Bag());
+    }
+
+    @Test(expected = UnknownOptionException.class)
+    public void should_retrieval_fail_when_Llocker_retrieval_bag_given_Lokcer() {
+        LLocker lLocker = new LLocker(1);
+
+        lLocker.retrieval(new Ticket());
     }
 }
